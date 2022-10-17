926905ad57c30d9bc7f0a9f9123228db64b0acb1
Oleksii Bielov <alexey.belov@gmail.com>
2022-10-17 17:57:56 +0200

    initial commit

---
+++ Python UDFs/PY_LOAD_SHAPEFILE.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION PY_LOAD_SHAPEFILE(PATH_TO_FILE string)
+returns table (wkb binary, properties object)
+language python
+runtime_version = 3.8
+imports=('@geostage/shapefiles_archive.zip')
+packages = ('fiona', 'shapely')
+handler = 'ShapeFileReader'
+AS $$
+import fiona
+from shapely.geometry import shape
+import sys
+IMPORT_DIRECTORY_NAME = "snowflake_import_directory"
+import_dir = sys._xoptions[IMPORT_DIRECTORY_NAME]
+
+class ShapeFileReader:        
+    def process(self, PATH_TO_FILE: str):
+      shapefile = fiona.open(f"zip://{import_dir}/shapefiles_archive.zip/{PATH_TO_FILE}")
+      for record in shapefile:
+        yield ((shape(record['geometry']).wkb, dict(record['properties'])))
+$$;
+
+SELECT * FROM table(PY_LOAD_SHAPEFILE('<PATH_TO_FILE>'));
